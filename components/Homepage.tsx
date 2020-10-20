a3449fd138c3430ff9ced0378377aeef2d58f243
Mocktar <44744049+mocktarissa@users.noreply.github.com>
2020-10-20 13:09:28 +0300

    fetching companies from API

--- components/Homepage.tsx
+++ components/Homepage.tsx
@@ -1,27 +1,38 @@
-import React, { Component, useState } from "react";
+import React, { Component, useState, useEffect } from "react";
 
-import { Text, View, StyleSheet, Button } from "react-native";
+import { Text, View, StyleSheet, Button, FlatList } from "react-native";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import axios from "axios";
+import Loading from "./Loading";
 export default function Homepage({ navigation }) {
-  const [places, setPlaces] = useState([
-    {
-      name: "Viasea",
-      location: "112112",
-      id: 1,
-    },
-  ]);
+  const [places, setPlaces] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        "http://myguideapi.herokuapp.com/api/company/"
+      );
+      setPlaces(result.data);
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
   function handleChange(newValue) {
     setPlaces(newValue);
   }
 
   return (
     <View style={styles.container}>
-      <Text>Hello from the Home Page </Text>
-      {places.map((p) => (
-        <Text key={p.id}>{p.name}</Text>
-      ))}
+      <Text>{places.length}</Text>
+      {isLoading ? <Text> Loading</Text> : <Text>Loaded</Text>}
+      <FlatList
+        data={places}
+        renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
+      />
+
       <View style={styles.bottom}>
         <Button
           title="Scan QR"
@@ -40,8 +51,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-
     alignItems: "center",
+  },
+  listRow: {
+    position: "absolute",
+    height: 20,
   },
   title: {
     textAlign: "center",
@@ -68,5 +82,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "flex-end",
     marginBottom: 36,
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
   },
 });
